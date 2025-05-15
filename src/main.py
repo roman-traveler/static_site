9f704e6891ef72052eb2cdfb1cda2a787a7d93c8
Bettinelli, Romain <Romain.Bettinelli@evotec.com>
2025-05-15 13:05:59 +0200

    recursive generation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from os_utils import copy_src_dir_to_dst_dir
-from page_generator import generate_page
+from page_generator import generate_page, generate_pages_recursive
 from textnode import TextNode, TextType
 
 
 def main():
     copy_src_dir_to_dst_dir("static","public")
+    generate_pages_recursive(
+        dir_path_content="content",
+        template_path="template.html",
+        dest_dir_path="public"
+    )
     generate_page(
         from_path="content/index.md",
         template_path="template.html",
--- src/page_generator.py
+++ src/page_generator.py
@@ -1,6 +1,21 @@
 from blocks import markdown_to_html_node
 from splitting_utils import extract_title
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    """
+    Recursively generate pages from markdown files in the given directory.
+    """
+    import os
+    for root, _, files in os.walk(dir_path_content):
+        for file in files:
+            if file.endswith(".md"):
+                from_path = os.path.join(root, file)
+                relative_path = os.path.relpath(from_path, dir_path_content)
+                dest_path = os.path.join(dest_dir_path, os.path.dirname(relative_path), "index.html")
+                dest_dir = os.path.dirname(dest_path)
+                if not os.path.exists(dest_dir):
+                    os.makedirs(dest_dir)
+                generate_page(from_path, template_path, dest_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} using template {template_path} to {dest_path}")
@@ -17,3 +32,4 @@ def generate_page(from_path, template_path, dest_path):
     print(template)
     with open(dest_path, "w") as f:
         f.write(template)
+
